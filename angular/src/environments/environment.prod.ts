40c43eacbf22b6eec72dfe47d20bb4dbbb57ebf4
Long Nguyen <43444536+hahpuc@users.noreply.github.com>
2023-01-08 21:31:30 +0700

    Feat: Config Docker env

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'http://localhost:4200';
+const baseUrl = 'http://89.117.53.210:4200';
 
 export const environment = {
   production: true,
@@ -10,7 +10,7 @@ export const environment = {
     logoUrl: '',
   },
   oAuthConfig: {
-    issuer: 'https://localhost:44342/',
+    issuer: 'http://89.117.53.210:44342/',
     redirectUri: baseUrl,
     clientId: 'Docker_App',
     responseType: 'code',
@@ -19,7 +19,7 @@ export const environment = {
   },
   apis: {
     default: {
-      url: 'https://localhost:44342',
+      url: 'http://89.117.53.210:44342',
       rootNamespace: 'LongLearn.Docker',
     },
   },
